c43be86882c2feb3b8c4723956a3d0c850dde3db
voskanvl <voskanvl@gmail.com>
2023-10-05 10:00:48 +0300

    Specter

---
+++ src/data/partners.ts
@@ -0,0 +1,10 @@
+import p1 from "@images/partners/Group 1337.png"
+import p2 from "@images/partners/Group 1338.png"
+import p3 from "@images/partners/Group 1339.png"
+import p4 from "@images/partners/Group 1340.png"
+import p5 from "@images/partners/Group 1341.png"
+import p6 from "@images/partners/Group 1342.png"
+import p7 from "@images/partners/Group 1343.png"
+import p8 from "@images/partners/Group 1344.png"
+
+export default [p1, p2, p3, p4, p5, p6, p7, p8]
