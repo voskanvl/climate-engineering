9c0304ff4606ac0e786bb2bf08a13ad5416a4052
voskanvl <voskanvl@gmail.com>
2023-10-05 13:43:00 +0300

    About + Block + min fixes

---
+++ src/data/about.ts
@@ -0,0 +1,31 @@
+import Garantia from "@images/icons/about/warranty 1.png"
+import Speedometer from "@images/icons/about/speedometer 1.png"
+import Turn from "@images/icons/about/turn 1.png"
+import Lightbulb from "@images/icons/about/lightbulb 1.png"
+const data: JobsType[] = [
+    {
+        caption: "Гарантия",
+        href: "",
+        text: "На все ремонтные работы распространяется гарантия до 5-ти лет",
+        icon: Garantia,
+    },
+    {
+        caption: "Оперативность",
+        href: "",
+        text: "Наличие укомплектованных бригад позволяет выходить на объект в любое время",
+        icon: Speedometer,
+    },
+    {
+        caption: "Гибкость",
+        href: "",
+        text: "При необходимости подбираем аналоги с полным соответствием технических требований",
+        icon: Turn,
+    },
+    {
+        caption: "Находчивость",
+        href: "",
+        text: "Не отказываемся от сложных объектов, а находим варианты решений при любых ограничениях технических условий",
+        icon: Lightbulb,
+    },
+]
+export default data
