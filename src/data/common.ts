663386e2f8f6f0b9222d89c4ceed4146f5a2b0de
voskanvl <voskanvl@gmail.com>
2023-10-05 05:22:38 +0300

    remove unnecessary yml

---
+++ src/data/common.ts
@@ -0,0 +1,13 @@
+import white from "@images/logo.png"
+import dark from "@images/logo-dark.png"
+const data = {
+    address: "г. Пенза, ул. Набережная реки Мойки, 39",
+    jobtime: "ПН-ПТ с 9:00 до 18:00",
+    phone: "+7 (8412) 540-620",
+    email: "client@russiaws.ru",
+    logo: {
+        white: "../assets/images/logo.png",
+        dark: "../assets/images/logo-dark.png",
+    },
+}
+export default data
