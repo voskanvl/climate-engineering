ce6a0ea8cb0def548ffbda68415fa0c876da8b4c
voskanvl <voskanvl@gmail.com>
2023-10-06 20:01:24 +0300

    Actions

--- src/config/modalElents.ts
+++ src/config/modalElents.ts
@@ -10,3 +10,15 @@ export const MobileMenuEvents = {
     mobileMenuOpen: "mobilemenuopen",
     mobileMenuClose: "mobilemenuclose",
 }
+
+export const Actions = {
+    letModalShow() {
+        document.dispatchEvent(new CustomEvent(ModalEvents.modalShow))
+    },
+    letModalClose() {
+        document.dispatchEvent(new CustomEvent(ModalEvents.modalClose))
+    },
+    sendModalMessage(message: string) {
+        document.dispatchEvent(new CustomEvent(ModalEvents.modalMessage, { detail: { message } }))
+    },
+}
