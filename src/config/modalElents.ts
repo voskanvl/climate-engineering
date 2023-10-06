15e1a2759e208ca1d13832e9ca7b321124350fae
voskanvl <voskanvl@gmail.com>
2023-10-06 13:22:08 +0300

    Modal + FormMessage

---
+++ src/config/modalElents.ts
@@ -0,0 +1,7 @@
+export const ModalEvents = {
+    modalShow: "modalshow",
+    modalClose: "modalclose",
+    modalShowed: "modalshowed",
+    modalClosed: "modalclosed",
+    modalMessage: "modalmessage",
+}
